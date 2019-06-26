da05041800bfe4fd9e44a53d0c9f0b6b0f78fc00
RafSwiggers <rafswiggers@gmail.com>
2019-06-26 09:53:10 +0200

    add some more images

---
+++ memory-game/script/javascript.js
@@ -0,0 +1,12 @@
+const cardsArray = [{
+            name: 'cap',
+            img: './memory-game/images/shield.png',
+        },
+        {
+            name: "thor",
+            img: "'./memory-game/images/hammer.jpg",
+        },
+        {
+            name: 'iron-man',
+            img: "'./memory-game/images/reactor.png"
+        }
